40311da1940ff5695ce295a7cb1140045895c9c8
Felix Kleine Bösing <felix.boesing@t-online.de>
2019-06-18 09:19:59 +0200

    moved helper functions

--- src/models/networks/NeuralNetwork.py
+++ src/models/networks/NeuralNetwork.py
@@ -5,7 +5,7 @@ from typing import Union, Tuple
 
 
 from src.models.Model import Model
-from src.models.networks.ActivationFunctions import sigmoid, softmax, linear, relu, tanh
+from src.models.networks.NetworkHelper import return_activation_functions
 
 
 class NeuralNetwork(Model):
@@ -54,43 +54,3 @@ class NeuralNetwork(Model):
             }
             last_neuron = neuron
         return variables
-
-
-def return_activation_functions(activ_funcs: list):
-    """
-    maps strings to functions. If function delivered in list it will be checked whether it outputs the right shape
-    :param activ_funcs: list of activation functions (str or functions)
-    :return: a list of functions
-    """
-    default_funcs = {"relu": relu, "tanh": tanh, "sigmoid": sigmoid, "linear": linear, "softmax": softmax}
-    funcs = []
-    for func in activ_funcs:
-        if isinstance(func, str):
-            funcs.append(default_funcs[func])
-        else:
-            _check_validity(func)
-            funcs.append(func)
-
-    return funcs
-
-
-def _check_validity(func: types.FunctionType):
-    test_data1 = np.array([1, 2, 3])
-    test_data2 = np.array([[1, 2, 3]])
-    test_data3 = np.array([[[1, 2, 3]]])
-
-    result_data1 = func(test_data1)
-    result_data2 = func(test_data2)
-    result_data3 = func(test_data3)
-
-    assert result_data1.shape == test_data1.shape, "Function {} is not valide, because shape differs from input to " \
-                                                   "output. Inputshape: {}, Outputshape: {}". \
-        format(func.__name__, test_data1.shape, result_data1.shape)
-    assert result_data1.shape == test_data1.shape, "Function {} is not valide, because shape differs from input to " \
-                                                   "output. Inputshape: {}, Outputshape: {}". \
-        format(func.__name__, test_data2.shape, result_data2.shape)
-    assert result_data2.shape == test_data2.shape, "Function {} is not valide, because shape differs from input to " \
-                                                   "output. Inputshape: {}, Outputshape: {}". \
-        format(func.__name__, test_data3.shape, result_data3.shape)
-
-
